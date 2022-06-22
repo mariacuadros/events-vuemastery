53c71426cd4283172cdcdaa1468110977c6588ce
Maria del Mar Cuadros <mariadelmar.cuadros@zemoga.com>
2022-06-22 12:06:12 -0500

    Create events from father component with data inputs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="home">
-    <EventCard />
+  <h1> Events for good </h1>
+  <div class="events-container">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
@@ -13,5 +14,40 @@ export default {
   components: {
     EventCard,
   },
+  data() {
+    return {
+      events: [
+        {
+          id: 59648,
+          title: 'Cat adoption',
+          time: '12:00pm',
+          date: 'January 20, 2022',
+        },
+        {
+          id: 59668,
+          title: 'Cat adoption',
+          time: '12:00pm',
+          date: 'January 20, 2022',
+        },
+        {
+          id: 58648,
+          title: 'Cat adoption',
+          time: '12:00pm',
+          date: 'January 20, 2022',
+        },
+      ],
+
+    };
+  },
+
 };
 </script>
+
+<style scoped>
+.events-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
