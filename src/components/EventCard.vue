f98f159ac590972d45100bb876294d38de0096b5
Maria del Mar Cuadros <mariadelmar.cuadros@zemoga.com>
2022-06-23 12:05:22 -0500

    BEFORE JSON ALL GOOD

---
+++ src/components/EventCard.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div class="event-card">
+      <p class="event-card__date"> @ {{ event.time}} on {{ event.date }} </p>
+      <p class="event-card__description"> {{ event.title }} </p>
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'EventCard',
+
+  props: {
+    event: Object,
+  },
+
+};
+</script>
+
+<style scoped>
+
+.event-card {
+  border: 1px solid black;
+  padding: 2em;
+  width: 30%;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1.5em;
+
+;
+}
+.event-card:hover{
+transform: scale(1.01);
+box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-card__description{
+  font-size: 1.5em;
+  font-weight: bold;
+}
+
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1> Events for good </h1>
+  <div class="events-container">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+// import axios from 'axios';
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 123,
+          title: 'Cat Adoption Day',
+          date: 'January 28, 2022',
+          time: '12:00',
+        },
+        {
+          id: 123,
+          title: 'Cat Adoption Day',
+          date: 'January 28, 2022',
+          time: '12:00',
+        },
+        {
+          id: 123,
+          title: 'Cat Adoption Day',
+          date: 'January 28, 2022',
+          time: '12:00',
+        },
+      ],
+
+    };
+  },
+
+};
+</script>
+
+<style scoped>
+.events-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
